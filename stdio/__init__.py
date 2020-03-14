923d3a3a29237e828526414eec7d1fc06abaa05e
Bhupendra Singh <bhsingh@gmail.com>
2020-03-14 09:43:00 +0530

    Serve static files

--- stdio/__init__.py
+++ stdio/__init__.py
@@ -2,11 +2,21 @@ import ssl
 import socket
 
 
-def connect(ip, port, cmd):
-    sock = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-    sock.connect((ip, port))
+class Cmd():
+    def __init__(self, ip, port, cmd):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock = ssl.wrap_socket(self._sock)
+        self._sock.connect((ip, port))
 
-    sock.sendall('cmd /{} http/1.0\n\n'.format(cmd).encode())
-    assert('HTTP/1.0 200 OK\n\n' == sock.recv(17).decode())
+        self.stdin = self._sock.makefile('w')
+        self.stdout = self._sock.makefile('r')
 
-    return sock
+        self.stdin.write('cmd /{} http/1.0\n\n'.format(cmd))
+        self.stdin.flush()
+
+        for line in self.stdout:
+            if not line.strip():
+                break
+
+    def __del__(self):
+        self._sock.close()
--- stdio/__main__.py
+++ stdio/__main__.py
@@ -2,12 +2,14 @@ import os
 import sys
 import ssl
 import runpy
+import signal
 import socket
 import logging
 import argparse
+import mimetypes
 import urllib.parse
 
-from . import connect
+from . import Cmd
 from logging import critical as log
 
 
@@ -19,7 +21,10 @@ def logfile(filename):
     os.dup2(os.open(name, os.O_CREAT | os.O_WRONLY | os.O_APPEND, 0o644), 2)
 
 
-def server(addr):
+def server(conn, addr):
+    sys.stdin = conn.makefile('r')
+    sys.stdout = conn.makefile('w')
+
     line = urllib.parse.unquote(sys.stdin.readline())
     sys.argv = line.split()[1:-1]
     sys.argv[0] = sys.argv[0][1:]
@@ -34,16 +39,36 @@ def server(addr):
 
     log('from%s cmd(%s)', addr, ' '.join(sys.argv))
 
-    logfile(sys.argv[0])
+    print('HTTP/1.0 200 OK')
 
-    print('HTTP/1.0 200 OK\n')
-    sys.stdout.flush()
-    runpy.run_module(sys.argv[0], run_name='__main__')
-    sys.stdout.flush()
+    if 1 == len(sys.argv) and '/' in sys.argv[0]:
+        mime_type = mime.guess_type(sys.argv[0])[0]
+        mime_type = mime_type if mime_type else 'application/octet-stream'
+        print('Content-Type: {}\n'.format(mime_type))
+        sys.stdout.flush()
+
+        with open(os.path.join(os.getcwd(), sys.argv[0]), 'rb') as fd:
+            length = 0
+            while True:
+                buf = fd.read(2**20)
+                if not buf:
+                    break
+
+                conn.sendall(buf)
+                length += len(buf)
+
+            log('client%s file(%s) bytes(%d)', addr, sys.argv[0], length)
+    else:
+        print()
+        sys.stdout.flush()
+        logfile(sys.argv[0])
+        runpy.run_module(sys.argv[0], run_name='__main__')
+        sys.stdout.flush()
 
 
 def main():
     logging.basicConfig(format='%(asctime)s %(process)d : %(message)s')
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
 
     if args.logdir and not os.path.isdir(args.logdir):
         os.mkdir(args.logdir)
@@ -63,24 +88,8 @@ def main():
         conn.close()
 
     sock.close()
-    conn = ssl.wrap_socket(conn, None, 'ssl.cert', True)
-    sys.stdin = conn.makefile('r')
-    sys.stdout = conn.makefile('w')
-    server(addr)
 
-
-def cmd(ip, port, cmd):
-    sock = connect(ip, port, cmd)
-
-    if not os.isatty(0):
-        sock.sendall(sys.stdin.read().encode())
-
-    while True:
-        buf = sock.recv()
-        if not buf:
-            break
-
-        os.write(1, buf)
+    server(ssl.wrap_socket(conn, None, 'ssl.cert', True), addr)
 
 
 if __name__ == '__main__':
@@ -93,7 +102,14 @@ if __name__ == '__main__':
     args.add_argument('--logdir', dest='logdir', default='')
     args = args.parse_args()
 
-    if args.cmd:
-        cmd(args.ip, args.port, args.cmd)
+    if args.ip and args.port and args.cmd:
+        cmd = Cmd(args.ip, args.port, args.cmd)
+
+        if not os.isatty(0):
+            cmd.stdin.write(sys.stdin.read())
+
+        while os.write(1, cmd.stdout.read().encode()):
+            pass
     else:
+        mime = mimetypes.MimeTypes()
         main()
